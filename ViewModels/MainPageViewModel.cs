558f3c7608970c6e140254ba74b14dd5b76b03b7
ntapp <maybeeonce@gmail.com>
2022-11-10 20:10:42 +0300

    Added base commands classes

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -4,6 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Reminder.Commands;
 using Reminder.Models;
 
 namespace Reminder.ViewModels
@@ -11,5 +14,15 @@ namespace Reminder.ViewModels
     public class MainPageViewModel : ViewModelBase
     {
         private readonly ObservableCollection<ReminderModel> _reminders;
+
+        private ICommand addCommand { get; }
+        private ICommand deleteCommand { get; }
+
+        public MainPageViewModel()
+        {
+
+            addCommand = new AddCommand();
+            deleteCommand = new DeleteCommand();
+        }
     }
 }
