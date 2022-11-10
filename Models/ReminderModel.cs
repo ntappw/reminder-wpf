4478fa4895bd45e1c9901e89045ddf3f334745f3
ntapp <maybeeonce@gmail.com>
2022-11-10 20:02:00 +0300

    Added base structure of program

---
+++ Models/ReminderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.Models
+{
+    public class ReminderModel
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public DateTime DateAndTime { get; }
+    }
+}
---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reminder.Models;
+
+namespace Reminder.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<ReminderModel> _reminders;
+    }
+}
