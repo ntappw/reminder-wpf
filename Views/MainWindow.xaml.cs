9ea66b423c5072e8d0d54b8902b0f1d795ca0c9c
ntapp <maybeeonce@gmail.com>
2022-11-11 19:29:51 +0300

    Added base view

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Reminder.ViewModels;
 
 namespace Reminder
 {
@@ -23,6 +24,7 @@ namespace Reminder
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainPageViewModel();
         }
     }
 }
